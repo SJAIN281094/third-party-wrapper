5b75b05242c34d7712029610be8e608e29e67bb1
SJAIN281094 <37086023+SJAIN281094@users.noreply.github.com>
2022-10-03 00:39:51 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,25 +1,9 @@
 const config = {
-  DATABASE: {
-    url: "mongodb+srv://shubhamjain:hiring123@cluster0.ygqo6.mongodb.net/hiringPortal?retryWrites=true&w=majority",
+  REDIS: {
+    host: "127.0.0.1",
+    post: 6379,
+    password: "",
   },
-  AWS: {
-    accessKeyId: "AKIAYG3PPFJWRXADXOJY",
-    secretAccessKey: "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",
-    region: "ap-south-1",
-    signatureVersion: "v4",
-    apiVersions: {
-      s3: "2006-03-01",
-    },
-    s3: {
-      bucketName: "hiring",
-    },
-  },
-  JWT: {
-    secret: "1234576890",
-    expiry: "86400s",
-    algorithms: ["HS256"],
-  },
-  DEFAULT_RANDOM_ID_GENERATOR_LENGTH: 12,
 };
 
 module.exports = config;

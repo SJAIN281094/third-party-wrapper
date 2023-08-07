54938b2cef7c3375ac7f995f0aeabed9416a2f01
Shubham <er.sjain28@gmail.com>
2023-08-07 22:05:15 +0530

    add prometheus

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ const RabbitMq = require("./rabbitMQ");
 const Logger = require("./logger");
 const Mailer = require("./email");
 const AccessLogger = require("./accessLogger");
+const MetricsClient = require("./prometheus");
 
-module.exports = { RedisIO, Logger, AccessLogger, RabbitMq, Mailer };
+module.exports = {
+  RedisIO,
+  Logger,
+  AccessLogger,
+  RabbitMq,
+  Mailer,
+  MetricsClient,
+};
